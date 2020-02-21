7d83b584c712ad6098b4b34f74983eb9596b2a45
Thuan Tigga Bellass <58609160+Mr-Thirteen-13@users.noreply.github.com>
2020-02-22 02:48:22 +0700

    Update gabutkan.py

--- gabutkan.py
+++ gabutkan.py
@@ -12,13 +12,13 @@ banner=("""\033[1;36m
   |_  ..  _|
   |_      _| \033[1;31mEmail =>Achenkbrankem218@gmail.com\033[1;36m
     |_||_|   \033[1;31mGithub=>https://github.com/Mr-Thirteen-13
-  \033[1;36mI LOVE U: \033[1;31mNitah Sari
+  \033[1;36mI LOVE U:  \033[1;31mNitah Sari
 """)
 def no():
 	try:
 		print(banner)
 		nama = input("\033[1;37mNama Anda =>\033[1;32m")
-		tgl = input("\033[1;37mTanggal Lahir(ex:21 04 2000) =>\033[1;32m").replace(' ','-')
+		tgl = input("\033[1;37mTanggal Lahir(ex:23 06 2002) =>\033[1;32m").replace(' ','-')
 		r = requests.get('https://script.google.com/macros/exec?service=AKfycbw7gKzP-WYV2F5mc9RaR7yE3Ve1yN91Tjs91hp_jHSE02dSv9w&nama='+nama,'&tanggal='+tgl)
 		jsn = json.loads(r.text)
 		a = jsn['data']
